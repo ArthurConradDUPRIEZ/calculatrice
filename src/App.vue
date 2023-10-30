41ef8618fb6e830b2a39b61a7537b54d9fa5178c
Arthur-Conrad <arthur59174@hotmail.fr>
2023-10-30 22:02:54 +0100

    Ajout du dark mode, amélioration des touches clv

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,282 @@
 <template>
-  <main>
-      <div class="display">
-          <div class="expression">{{ expression }}</div>
-          <div class="result">{{ result }}</div>
+  <div :class="theme ? 'dark_mode' : 'light_mode'" >
+    <header>
+      <div id="switchTheme">
+        <va-switch
+          v-model="theme"
+          size="large"
+          color="#5123a1"
+          off-color="#ffd300"
+          style="--va-switch-checker-background-color: #252723;"
+        >
+          <template #innerLabel>
+            <div class="va-text-center">
+              <va-icon
+                :name="theme ? 'dark_mode' : 'light_mode'"
+              />
+            </div>
+          </template>
+        </va-switch>
       </div>
-      <div class="pave">
-          <input class="pavButton" type="button" value="%" @click="pourcent">
-          <input class="pavButton" type="button" value="CE" @click="save">
-          <input class="pavButton" type="button" value="C" @click="del">
-          <input class="pavButton" type="button" value="<-" @click="remove">
-          <input class="pavButton" type="button" value="1/x" @click="fraction">
-          <input class="pavButton" type="button" value="x²" @click="puissance">
-          <input class="pavButton" type="button" value="val" @click="recup">
-          <input class="pavButton" type="button" value="/" @click="write('/')" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="7" @click="write(7)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="8" @click="write(8)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="9" @click="write(9)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="*" @click="write('*')" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="4" @click="write(4)" @keydown="handleKeydown">            
-          <input class="pavButton" type="button" value="5" @click="write(5)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="6" @click="write(6)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="-" @click="write('-')" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="1" @click="write(1)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="2" @click="write(2)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="3" @click="write(3)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="+" @click="write('+')" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="+/-" @click="write(7)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="0" @click="write(0)" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="." @click="write('.')" @keydown="handleKeydown">
-          <input class="pavButton" type="button" value="=" @click="calculation" @keydown="handleKeydown">
-      </div>   
-  </main>
-  
+    </header>
+    <main>
+      <div id="calculatruice">
+        <div class="display">
+            <div class="expression">{{ expression }}</div>
+            <div class="result">{{ result }}</div>
+        </div>
+        <div class="pave">
+          <input v-for="item in listInput" type="button" class="pavButton" :value="item.button" @click="item.function">
+        </div> 
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup>
 
-import {ref} from 'vue';
+import { ref, onMounted, watch } from 'vue';
 
 const result = ref(0);
 const savedExpression = ref('');
 const expression = ref('');
+const theme = ref(false);
 
-// fonction permettant d'ecrire le la valeur ou l'opération désiré
-function write(n) {
+function write(n) {                                                     // fonction permettant d'ecrire le la valeur ou l'opération désiré
   expression.value = expression.value + n
 }
-function remove(){
+function remove(){                                                      // fonction permettant de supprimer le dernier terme inscrit
   expression.value = expression.value.slice(0, -1);
 }
-function del(){
+function del(){                                                         // fonction remettant à 0 l'expression 
   expression.value = '';
 }
-function calculation(){
+function calculation(){                                                 // fonction qui calcule l'espression et l'affiche dans le resultat
   result.value = eval(expression.value);
   savedExpression.value = expression.value;
 }
-function save(){
+function save(){                                                        // fonction permettant de revenir à la dernière expression non calculée
   expression.value = savedExpression.value;
 }
-function puissance(){
+function puissance(){                                                   // fonction permettant de calculer le carré de l'expression
   result.value = eval(expression.value) * eval(expression.value);
   expression.value = ''
 }
-function pourcent(){
+function pourcent(){                                                    // fonction permettant d'afficher l'expression en pourcentage
   result.value = eval(expression.value) / 100;
   expression.value = ''
 }
-function fraction(){
+function fraction(){                                                    // fonction permettant de calculer l'inverse de l'expression
   result.value =  1 / eval(expression.value);
   expression.value = ''
 }
-function recup(){
+function recup(){                                                       // fonction permettant de recupérer le résultat et de le réintroduire dans l'expression
   expression.value = result.value.toString();
 }
+function signe(){                                                       // fonction permettant de modifier le signe de l'expression
+  result.value = eval(expression.value) * -1
+}
 
+const listInput = ref([
+  {
+    button: '%',
+    function: pourcent
+  },
+  {
+    button: 'CE',
+    function: save
+  },
+  {
+    button: 'C',
+    function: del
+  },
+  {
+    button: '<-',
+    function: remove
+  },
+  {
+    button: '1/x',
+    function: fraction
+  },
+  {
+    button: 'x²',
+    function: puissance
+  },
+  {
+    button: 'val',
+    function: recup
+  },
+  {
+    button: '/',
+    function: () => write('/')
+  },
+  {
+    button: '7',
+    function: () => write(7)
+  },
+  {
+    button: '8',
+    function: () => write(8)
+  },
+  {
+    button: '9',
+    function: () => write(9)
+  },
+  {
+    button: '*',
+    function: () => write('*')
+  },
+  {
+    button: '4',
+    function: () => write(4)
+  },
+  {
+    button: '5',
+    function: () => write(5)
+  },
+  {
+    button: '6',
+    function: () => write(6)
+  },
+  {
+    button: '-',
+    function: () => write('-')
+  },
+  {
+    button: '1',
+    function: () => write(1)
+  },
+  {
+    button: '2',
+    function: () => write(2)
+  },
+  {
+    button: '3',
+    function: () => write(3)
+  },
+  {
+    button: '+',
+    function: () => write('+')
+  },
+  {
+    button: '+/-',
+    function: signe
+  },
+  {
+    button: '0',
+    function: () => write(0)
+  },
+  {
+    button: '.',
+    function: () => write('.')
+  },
+  {
+    button: '=',
+    function: calculation
+  },
+  
+  
+]);
+
+
+function showClick(key){                                                  // fonction permettant d'afficher la touche qui a été enfoncer sur le clavier
+  const buttonSelected = document.querySelector(`input[value="${key}"]`); // on recupere le bouton correspondant à la touche de clavier appuyée
+  buttonSelected.classList.add('clicked');                                // on ajoute la class clicked qui possède certains style
+  buttonSelected.click();                                                 // on click sur la touche désirée
+  setTimeout(() =>{                                                       // et on enlève la class clicked 0.2 sec après 
+    buttonSelected.classList.remove('clicked');                           
+  }, 200)
+}
 
 
 function handleKeydown(event) {
-  if (event.keyCode === 13) {
-    // La touche "Entrée" du clavier principal
-    console.log('La touche "Entrée" a été enfoncée !');
-    calculation();
+  const key = event.key;
+
+  if (key === 'Enter') {
+    showClick('=');
   } 
-  else if (event.keyCode >= 96 && event.keyCode <= 105) {
-    // Touches du pavé numérique de 0 à 9
-    write(event.keyCode - 96);
+  else if (key >= '0' && key <= '9') {
+    // const buttonSelected = document.querySelector(`input[value="${key}"]`);
+    showClick(key);
+  } 
+  else if (key === 'Backspace') {
+    showClick('<-')
+  } 
+  else if (key === '*') {
+    showClick(key);
+  } 
+  else if (key === '+') {
+    showClick(key);
+  } 
+  else if (key === '-') {
+    showClick(key);
+  } 
+  else if (key === '.') {
+    showClick(key);
+  } 
+  else if (key === '/') {
+    showClick(key);
   }
-  else if (event.keyCode === 8){ //quand la touche back est appuyée
-      remove();
+  else if (key === 'ArrowUp') {
+    showClick('val')
   }
-  else if(event.keyCode === 106){ //quand la touche * est appuyée
-      write('*'); 
+  else if (key === 'Delete') {
+    showClick('C')
   }
-  else if(event.keyCode === 107){ //quand la touche + est appuyée
-      write('+');
+  else if (key === 'Escape') {
+    showClick('CE')
   }
-  else if(event.keyCode === 109){ //quand la touche - est appuyée
-      write('-');
+  else if (key === '²') {
+    showClick('x²')
   }
-  else if(event.keyCode === 110){  //quand la touche , est appuyée
-      write('.')
+  else if (key === '%') {
+    showClick(key)
   }
-  else if(event.keyCode === 111){ //quand la touche / est appuyée
-      write('/');
-  }
-
-
-
-
-
-
 }
 
+function changeTheme(newTheme){
+  console.log('Le thème a changé : ', newTheme);
+};
 
+watch(theme, changeTheme)
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeydown);
+
+});
 
 
 </script>
 
 <style>
 
-main{
+header{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 10px;
+}
+
+.light_mode{
+  padding: 10px;
+  background-color: rgb(221, 220, 220);
+  height: 100vh;
+}
+#switchTheme{
+  margin: 0 10px ;
+  height: 100%;
+}
+
+#calculatruice{
   position: absolute;
   left: 25%;
   top: 5%;
   width: 50%;
   min-height: 400px;
   height: 90vh;
-  border: 1px solid black;
+  border: 3px solid black;
   border-radius: 10px;
-  background-color: grey;
+  background-color: rgb(97, 114, 242);
 }
 
 .display{
@@ -138,7 +285,7 @@ main{
   min-height: 50px;
   padding: 5px;margin: 10px auto;
   background-color: white;
-  border: 1px solid darkslategrey;
+  border: 2px solid black   ;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
@@ -147,26 +294,30 @@ main{
 }
 .display *{
   min-height: 40%;
+  color: black;
+  font-weight: bold;
 }
 
 .pave{
-  border: 1px solid black;
+  border: 2px solid black;
   border-radius: 7px;
   width: 90%;
   height: 80%;
   margin: auto;
-  background-color: rgb(63, 63, 63);
   display: grid;
   grid-template-columns: repeat(4,1fr);
   grid-template-rows: repeat(6,1fr);
   grid-gap: 3px;
+  background-color: rgb(141, 153, 242);
 }
 
-.pavButton{
+.pave .pavButton{
   font-size: 36px;
   margin: 5px;
   border-radius: 15px;
+  border: 1px solid black;
   cursor: pointer;
+  background-color: rgb(196, 202, 245);
 }
 
 
@@ -176,9 +327,36 @@ main{
   background-color: rgb(231, 133, 42);
 }
 
-.pavButton:active{
+.pavButton:active, .pavButton.clicked{
   background-color: rgb(231, 102, 42);
 }
+
+.dark_mode{
+  background-color: #171010;
+  padding: 10px;
+  height: 100vh;
+}
+.dark_mode #calculatruice{
+  background-color: #362222;
+}
+.dark_mode .display *{
+  color: #B5B5B5;
+}
+ 
+.dark_mode .pave, .dark_mode .display{
+  background-color: #423F3E;
+}
+.dark_mode .pavButton{
+  background-color: #2B2B2B;
+  color: #bbbbbb;
+  border-color: #cccccc;
+}
+
+
+
+
+
+
 
 @media (max-width:700px){
 main{
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/dist/vuestic-ui.css";
+import "./index.css"; // Assurez-vous d'ajuster le chemin en fonction de l'emplacement de votre fichier CSS.
+
+
 
 const app = createApp(App)
 
 app.use(createPinia())
-
+app.use(createVuestic());
 app.mount('#app')
